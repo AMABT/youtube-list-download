1e7058543d226cad4d75f87da515646e30746ffa
Alecu Marian <a.marian.alexandru@gmail.com>
2017-01-05 14:06:21 +0200

    Remove DummyContent class

--- app/src/main/java/com/example/malecu/youtubelistdownload/VideoFragment.java
+++ app/src/main/java/com/example/malecu/youtubelistdownload/VideoFragment.java
@@ -11,9 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.malecu.youtubelistdownload.dummy.DummyContent;
-import com.example.malecu.youtubelistdownload.dummy.DummyContent.DummyItem;
-
 import java.util.ArrayList;
 import java.util.List;
 
